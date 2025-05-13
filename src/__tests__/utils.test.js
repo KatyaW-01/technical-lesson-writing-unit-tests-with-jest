cb1fe296aa30bd831217f8661f23ec43175d471e
Katya Weicht <katya.weicht@gmail.com>
2025-05-13 15:25:26 -0600

    Refactor function

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -2,7 +2,7 @@
 
 import { pointsForWord } from "../utils";
 
-describe("pointsForWrod", () => {
+describe("pointsForWord", () => {
   it("calculates the total points for a word (1 point per vowel, 2 per consonant)",() => {
     const word = "test";
     const points = pointsForWord(word);
